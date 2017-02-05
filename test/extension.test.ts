866179ee582041adfe704d60d1784b2f07c8717b
JR McEntee <jrmce12@gmail.com>
2017-02-05 14:19:39 -0500

    Adding tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,22 +1,45 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+describe('MoveOn Tests', () => {
+    let editor: vscode.TextEditor = vscode.window.activeTextEditor;
+    const lineZero = 'function example() {\n';
+    const lineOne = '\tconst string = \'my test string\';\n';
+    const lineTwo = '}'
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    before(() => {
+        editor = vscode.window.activeTextEditor;
+
+        return editor.edit((editBuilder: vscode.TextEditorEdit) => {
+            editBuilder.insert(new vscode.Position(0, 0), `${lineZero}${lineOne}${lineTwo}`);
+        });
+    });
+
+    it('moves on from single quotes', () => {
+        const line = editor.document.lineAt(1);
+        const position = new vscode.Position(1, line.text.length - 2);
+        editor.selection = new vscode.Selection(position, position);
+
+        vscode.commands.executeCommand('moveOn.execute');
+
+        const currentSelection: vscode.Position = editor.selection.active;
+
+        assert.equal(currentSelection.line, 1)
+        assert.equal(currentSelection.character, line.text.length - 1);
+    });
+
+    it('moves on from parenthesis', () => {
+        const line = editor.document.lineAt(0);
+        const position = new vscode.Position(0, line.text.length - 3);
+        editor.selection = new vscode.Selection(position, position);
+
+        vscode.commands.executeCommand('moveOn.execute');
+
+        const currentSelection: vscode.Position = editor.selection.active;
+
+        assert.equal(currentSelection.line, 0)
+        assert.equal(currentSelection.character, line.text.length - 2);
     });
 });
